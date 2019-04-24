4a9dc7a7f96bd4af51e47a54cc6667b3de9feb2b
Kevin Yang <yangk@31-36-24.wireless.csail.mit.edu>
2019-04-24 16:07:44 -0400

    bugfixes

--- scripts/compile_main2results.py
+++ scripts/compile_main2results.py
@@ -2,9 +2,19 @@ import os
 
 for folder in os.listdir('.'):
     try:
-        with open(os.path.join(folder, 'verbose.log'), 'r') as f:
-            for line in f:
-                last_line = line.strip()
-        print(folder, last_line)
+        if folder.endswith('time_window'):
+            for subfolder in os.listdir(folder):
+                try:
+                    with open(os.path.join(folder, subfolder, 'verbose.log'), 'r') as f:
+                        for line in f:
+                            last_line = line.strip()
+                    print(folder, subfolder, last_line)
+                except:
+                    print(folder, subfolder, 'failed')
+        else:
+            with open(os.path.join(folder, 'verbose.log'), 'r') as f:
+                for line in f:
+                    last_line = line.strip()
+            print(folder, last_line)
     except:
         print(folder, 'failed')
